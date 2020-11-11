5cebc08ef414ff029142e7ca4a661eafce2f8b31
Zhongyang MA <mzy@mail.ustc.edu.cn>
2020-11-11 11:31:50 +0800

    function component

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # react-exercise
+
 An Introduction to ReactJS.
+
+ - Diff 算法
+ - 虚拟 DOM
+
+JSX to everything 
+
+ - JSX --> HTML
+ - JSX --> native ios / android
+ - JSX --> VR
+ - JSX --> 物联网
+ 
+React的基本使用
+
+ - ReactDOM.render 渲染入口
+ 
+JSX的基本使用
+
+ - JavaScript XML
+ - Babel编译
+ 
+React组件
+
+ - 通过函数创建，函数组件， 无状态
+ - 通过类创建，类组件，有状态
+
+函数组件
+1. 函数名必须以大写字母开头，React 以此来判断是不是组件
+2. 函数必须有返回值，可以是 null 或 JSX 对象
+3. 返回的 JSX 必须有一个根元素
+4. 组件的返回值用（）包裹，避免换行问题
+
--- src/index.js
+++ src/index.js
@@ -2,29 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Demo extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Shopping list for {this.props.name}</h1>
-                <ul>
-                    <li>Aaaa</li>
-                    <li>Bbbb</li>
-                </ul>
-            </div>
-        )
-    }
+/**
+ * 函数组件
+ * 1. 函数名必须以大写字母开头，React 以此来判断是不是组件
+ * 2. 函数必须有返回值，可以是 null 或 JSX 对象
+ * 3. 返回的 JSX 必须有一个根元素
+ * 4. 组件的返回值用（）包裹，避免换行问题
+ */
+function Welcome(props) {
+    return (
+        <div>
+            <h1>函数式组件</h1>
+            <p>函数式组件：{props.name}</p>
+        </div>
+    )
 }
 
-// ===================
-
 ReactDOM.render(
-    <Demo name={"GGGG"}/>,
-    document.getElementById('root')
+    <Welcome name={"test"} />,
+    document.getElementById("root")
 );
